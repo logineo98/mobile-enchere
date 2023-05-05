61a97fe37290c4e7012759ed8ccfa3cd68bcc739
akzoft <akougnon.dolo.7@gmail.com>
2023-05-05 17:27:12 +0000

    3e commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StyleSheet } from 'react-native'
 import React, { useEffect } from 'react'
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 import RootNavigation from './src/libs/navigations/RootNavigation'
 import { Provider } from 'react-redux';
 import { Store } from './src/libs';
 
 
+
+
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={Store}><RootNavigation /></Provider>
   )
