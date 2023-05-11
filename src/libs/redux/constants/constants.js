a6d2bcc194e118c60f2b74b162bf07bdcfd4dc1c
Logineo <c.diabate@logineo.com>
2023-05-11 10:54:32 +0000

    modification dans constants 10.54

--- src/libs/redux/constants/constants.js
+++ src/libs/redux/constants/constants.js
@@ -1,6 +1,6 @@
 
-export const api = "http://192.168.50.82:5000"
-export const api_public = "http://192.168.50.82:5000/api/public"
+// export const api = "http://192.168.50.82:5000"
+// export const api_public = "http://192.168.50.82:5000/api/public"
 
 // export const api = "http://192.168.1.102:5000"
 // export const api_public = "http://192.168.1.102:5000/api/public"
@@ -18,9 +18,9 @@ export const api_public = "http://192.168.50.82:5000/api/public"
 // export const vitepay_api = "https://meyere.adaptable.app/vitepay/callback"
 
 // pour logineo
-// export const api = "https://api-enchere.adaptable.app"
-// export const api_public = "https://api-enchere.adaptable.app/api/public"
-// export const vitepay_api = "https://api-enchere.adaptable.app/api/vitepay/callback"
+export const api = "https://api-enchere.adaptable.app"
+export const api_public = "https://api-enchere.adaptable.app/api/public"
+export const vitepay_api = "https://api-enchere.adaptable.app/api/vitepay/callback"
 
 // pour tz
 // export const api = "https://api-meyere.adaptable.app"
