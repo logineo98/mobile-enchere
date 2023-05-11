5c8b1688eab8ee018117dd6bbb8a445ff72b3d68
akzoft <akougnon.dolo.7@gmail.com>
2023-05-11 16:25:18 +0000

    mobile

--- src/libs/redux/constants/constants.js
+++ src/libs/redux/constants/constants.js
@@ -13,14 +13,14 @@
 // export const api_public = "http://192.168.50.247:5000/api/public"
 
 
-// export const api = "https://meyere.adaptable.app"
-// export const api_public = "https://meyere.adaptable.app/api/public"
-// export const vitepay_api = "https://meyere.adaptable.app/vitepay/callback"
+export const api = "https://meyere-api.adaptable.app"
+export const api_public = "https://meyere-api.adaptable.app/api/public"
+export const vitepay_api = "https://meyere-api.adaptable.app/vitepay/callback"
 
 // pour logineo
-export const api = "https://api-enchere.adaptable.app"
-export const api_public = "https://api-enchere.adaptable.app/api/public"
-export const vitepay_api = "https://api-enchere.adaptable.app/api/vitepay/callback"
+// export const api = "https://api-enchere.adaptable.app"
+// export const api_public = "https://api-enchere.adaptable.app/api/public"
+// export const vitepay_api = "https://api-enchere.adaptable.app/api/vitepay/callback"
 
 // pour tz
 // export const api = "https://api-meyere.adaptable.app"
